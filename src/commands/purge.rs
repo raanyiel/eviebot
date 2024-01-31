eece3c43f490bc34eb28e98034c74145f240a660
raanyiel <cgunnar05@gmail.com>
2024-01-30 20:56:50 -0600

    fix: :safety_vest: Added checks to purge

--- src/commands/purge.rs
+++ src/commands/purge.rs
@@ -8,7 +8,11 @@ use serenity::{
 #[usage = "<number_of_messages>"]
 #[required_permissions(MANAGE_MESSAGES)]
 async fn purge(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    let num_messages: u8 = args.single::<u8>()?;
+    let num_messages: i32 = args.single::<i32>()?;
+    if num_messages > 100 || num_messages < 0 {
+        msg.channel_id.say(&ctx.http, "Nuh uh.").await?;
+        return Ok(())
+    }
     let history = msg.channel_id.messages_iter(&ctx).boxed();
     let mut messages = history.take(num_messages as usize + 1);
     while let Some(message_result) = messages.next().await {
