ded3aa74ed90ffe0b713933ddbc98e00a3b85964
raanyiel <cgunnar05@gmail.com>
2024-01-30 21:26:05 -0600

    feat: :beers: added error feedback

--- src/commands/purge.rs
+++ src/commands/purge.rs
@@ -8,13 +8,21 @@ use serenity::{
 #[usage = "<number_of_messages>"]
 #[required_permissions(MANAGE_MESSAGES)]
 async fn purge(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    let num_messages: i32 = args.single::<i32>()?;
-    if num_messages > 100 || num_messages < 0 {
+    let num_messages = args.single::<i32>();
+    let mewo = match num_messages {
+        Ok(num) => num,
+        Err(_err) => {
+            msg.channel_id.say(&ctx.http, "are you dumb").await?;
+            return Ok(())
+        }
+    };
+
+    if mewo > 100 || mewo < 0 {
         msg.channel_id.say(&ctx.http, "Nuh uh.").await?;
         return Ok(())
     }
     let history = msg.channel_id.messages_iter(&ctx).boxed();
-    let mut messages = history.take(num_messages as usize + 1);
+    let mut messages = history.take(mewo as usize + 1);
     while let Some(message_result) = messages.next().await {
         match message_result {
             Ok(message) => message.delete(&ctx.http).await.unwrap_or_else(|err| eprintln!("meow: {}", err)),
